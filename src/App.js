048399339d79cb8a08863ca25acd157e441ced1e
dunnoconnor <michael.dunn.oconnor@gmail.com>
2023-08-09 15:11:28 -0400

    card style and dark mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import PokemonCard from './components/PokemonCard';
 import { useEffect,useState } from 'react';
 
-const pokemonAPI =
-  'https://pokeapi.co/api/v2/pokemon?limit=151';
+const pokemonAPI = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
@@ -19,15 +26,18 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-        {pokemon.map((p,i) =>
-        <PokemonCard name={p.name} id={i+1}/>
-            )}
-        </Grid>
-      </Box>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+          {pokemon.map((p,i) =>
+          <PokemonCard name={p.name} id={i+1}/>
+              )}
+          </Grid>
+        </Box>
+      </div>
+  </ThemeProvider>
   );
 }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Grid from '@mui/material/Grid'
-import { Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, CardMedia } from '@mui/material';
 
 function PokemonCard({name, id}) {
     return (
-    <Grid item xs={4} key={id}>
-        <Typography variant="p">{name}</Typography>
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={name}></img>
+    <Grid item xs={4} md={3} lg={2} key={id}>
+        <Card sx={{ maxWidth: 240 }}>
+            <CardMedia
+                sx={{ height: 200, width: 200 }}
+                image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+                title={name}
+            />
+            <CardContent>
+                <CardActions>
+                    <Button>{name}</Button>
+                </CardActions>
+            </CardContent>
+        </Card>
+
     </Grid>
     );
 }
